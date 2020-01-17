013a662b2dead1d1fe8308dfcb5179f0f7fbf1be
dzmitry <kislodim@gmail.com>
2020-01-17 19:37:56 +0300

    05 End - Routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 import './menu-item.styles.scss'
+import {withRouter} from "react-router-dom";
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, linkUrl, history}) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(linkUrl)}>
         <div className="background-image"
              style={{
                  backgroundImage: `url(${imageUrl})`
@@ -15,4 +16,4 @@ const MenuItem = ({title, imageUrl, size}) => (
     </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
